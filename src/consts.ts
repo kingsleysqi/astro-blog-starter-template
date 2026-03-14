60508c790201c397c64b3e1a427389c5fbc8d266
Kingsley Qi <kingsley.s.qi@gmail.com>
2026-03-14 17:54:00 +0800

    更新 consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Kingsley Qi - KQ | Design & Technology Explorer";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_DESCRIPTION = "Kingsley Qi (KQ) - 设计与技术边界探索者 | 在锐利中寻圆润，在缺失中见完整。个人作品、思考与生活记录。";
